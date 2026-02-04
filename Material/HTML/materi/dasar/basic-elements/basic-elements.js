9878485a8fb2deedc6fed917ad6506a267479329
sulthonmufti <msulthonmufti14@gmail.com>
2026-02-05 00:37:25 +0700

    add materials basic HTML

---
+++ Material/HTML/materi/dasar/basic-elements/basic-elements.js
@@ -0,0 +1,9 @@
+function changePage() {
+            const selectedPage = document.getElementById('pageSelect').value;
+            const pages = document.querySelectorAll('.materi-content');
+            
+            pages.forEach(p => p.classList.remove('active'));
+            document.getElementById(selectedPage).classList.add('active');
+            
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
